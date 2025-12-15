48a1ecced2ba8c42be78dc3804c2960e4a12924e
ericwittrock <ericjwittrock@gmail.com>
2025-12-15 00:04:09 -0600

    sphere texture

--- src/sphere.h
+++ src/sphere.h
@@ -45,6 +45,32 @@ public:
         return true;
     }
 
+    __device__ static Vec3 randSphereVec(curandState* randState) 
+    {
+        float r1 = curand_uniform(randState);
+        float r2 = curand_uniform(randState);
+        float theta = r1 * 2.0f * 3.14159f;
+        float phi = acosf(2.0f * r2 - 1.0f);
+        float x = sinf(phi) * cosf(theta);
+        float y = sinf(phi) * sinf(theta);
+        float z = cosf(phi);
+        return Vec3(x, y, z);
+    }
+
+    __device__ static Vec3 getTextureColor(float u, float v, const float* textures, int image_id, const Material& mat) {
+        float px = u * mat.image_width;
+        float py = v * mat.image_height;
+        int offset = image_id == 0 ? mat.image1_offset : (image_id == 1 ? mat.image2_offset : mat.image3_offset);
+        if (offset < 0) return Vec3(1.0f, 0.0f, 1.0f); // the color of error
+
+        int i = offset + 3 * (int)px + 3 * mat.image_width * (int)py;
+        return Vec3(
+            textures[i],
+            textures[i + 1],
+            textures[i + 2]
+        );
+    }
+
     // spheres have limited material support
     // you can load a sphere as a mesh instead for full material options
     // This is just to meet the requirement of sphere intesections and textured spheres
@@ -52,10 +78,15 @@ public:
         float u = atan2(ray.direction.z, ray.direction.x) / (2.0f * 3.14159f) + 0.5f;
         float v = -asin(ray.direction.y) / (2.0f * 3.14159f) + 0.5f;
 
+        Vec3 clr = getTextureColor(u, v, textures, 0, mat);
+        ray.diffuseMultiplier = ray.diffuseMultiplier * clr;
         ray.position = hitPos;
-        ray.direction = normal;
+        Vec3 randomDir = randSphereVec(randState);
+        ray.direction = (normal + randomDir).normalize();
+
         ray.marchForward(1e-5f);
     }
+
 
 private:
     Vec3 center;
